f98d5b8bcea91e0b1abc81f39c941bd87b6521ca
Amr Aboelela <amraboelela@gmail.com>
2016-03-06 13:48:12 -0800

    Rolled back some changes

--- LauncherView.m
+++ LauncherView.m
@@ -36,24 +36,25 @@
         _applications = FileManagerInstantiateApps();
         NSSortDescriptor *scoreDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"score" ascending:NO];
         NSArray *scoreDescriptors = [NSArray arrayWithObjects:scoreDescriptor, nil];
-        scoreDescriptors = [NSArray arrayWithObjects:[NSSortDescriptor sortDescriptorWithKey:@"category" ascending:YES],[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES], nil];
-        int numberOfPages = [self numberOfPages];
+        
+        //ApplicationsData *applicationsData = [ApplicationsData sharedData];
+        int numberOfPages = [self numberOfPages];//applicationsData->_applicationsPages.count;
         PageView *pageView = nil;
         self.contentSize = CGSizeMake(theFrame.size.width * numberOfPages, theFrame.size.height);
         self.pagingEnabled = YES;
-        NSArray *homeApplications = [[[_applications sortedArrayUsingDescriptors:scoreDescriptors] subarrayWithRange:
-                                     NSMakeRange(0,PageViewNumberOfAppsPerPage())] sortedArrayUsingDescriptors:scoreDescriptors];
+        //DLog(@"numberOfPages: %d", numberOfPages);
+        //for (int i=0; i<numberOfPages; i++) {
         pageView = [[PageView alloc]
                     initWithFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y,
                                              self.frame.size.width, self.frame.size.height)
                     scrollView:self
-                    applications:homeApplications
+                    applications:[_applications sortedArrayUsingDescriptors:scoreDescriptors]
                     pageNumber:0];
         [self addSubview:pageView];
         [pageView release];
         //};
         
-
+        scoreDescriptors = [NSArray arrayWithObjects:[NSSortDescriptor sortDescriptorWithKey:@"category" ascending:YES],[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES], nil];
         self.applications = [_applications sortedArrayUsingDescriptors:scoreDescriptors];
         for (int i=1; i<numberOfPages; i++) {
             pageView = [[PageView alloc]
